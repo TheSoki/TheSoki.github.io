4d5854bd6a91521e7f59555d22918aadb04f14f0
Marek Sokol <devmareksokol@gmail.com>
2023-11-18 20:54:16 +0100

    feat: ✨ dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      extend: {
-        fontFamily: {
-          sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
-        },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
+  darkMode: 'class',
 };
